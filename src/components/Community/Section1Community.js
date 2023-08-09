4fbd2188582b4040cf251ce04e543e786963909a
sahil <sahilalagiya104@gmail.com>
2023-08-09 15:59:02 +0530

    new version

--- src/components/Community/Section1Community.js
+++ src/components/Community/Section1Community.js
@@ -2,15 +2,16 @@ import React from 'react'
 import community from '../../Images/community.jpg'
 import { AiFillCheckCircle } from 'react-icons/ai'
 import { BsArrowRightShort } from 'react-icons/bs'
+import '../../Aboutcss/gradient.css'
 
 const Section1Community = () => {
     return (
-        <div style={{ backgroundColor: '#E3E2F8' }}>
+        <div style={{ backgroundColor: '#E3E2F8', overflow:'hidden' }}>
             <div className='col-sm-9 col-lg-10 col-11 row mx-auto'>
-                <div className='mb-5 ' style={{  paddingTop: '100px' }} >
+                <div className='mb-2 ' style={{ paddingTop: '100px' }} >
                     <div className='row d-flex justify-content-center'>
                         <div className='col-lg-6 ' style={{ marginRight: '0px' }}>
-                            <h6  style={{ color: '#BD10E0' }} className='px-3'>COMMUNITY BUILDER</h6>
+                            <h6 style={{ color: '#BD10E0' }} className='px-3'>COMMUNITY BUILDER</h6>
                             <h1 style={{ color: 'black', borderLeft: '2px solid #BD10E0' }} className=' display-4 fw-bolder my-3 px-2 col-9 '>Build Your Community On Our Platform</h1>
                             <p className='my-3 px-3' style={{ color: 'black' }}>Community Builder is more than just a platform for interaction; it's a comprehensive suite of tools designed to help community leaders engage, educate, and grow their membership base. It enables you to showcase your trading results, strategies, and insights, providing transparent and compelling evidence of your success to attract and retain community members.</p>
                         </div>
@@ -19,8 +20,11 @@ const Section1Community = () => {
                         </div>
                     </div>
                 </div>
+                </div>
 
-                <div className='row d-flex justify-content-start ' style={{ marginTop: '50px' }}>
+                <div className="gradient-box my-5"></div>
+                <div className='col-sm-9 col-lg-10 col-11  mx-auto' >
+                <div className='row d-flex justify-content-start ' style={{ marginTop: '100px' }}>
                     <div className='col-sm-9 col-11 ' style={{ marginRight: '0px' }}>
                         <h6 className='px-3' style={{ color: '#BD10E0' }}>GET CONTROL OF YOUR COMMUNITY</h6>
                         <h1 style={{ color: 'black', borderLeft: '2px solid #BD10E0' }} className=' fw-bolder my-3 px-2 col-lg-9 col-11 '>Integrated Community Builder with AITradingSpy Platform</h1>
@@ -58,12 +62,25 @@ const Section1Community = () => {
 
                 </div>
 
-                <div className='d-flex justify-content-center row px-3 py-5' style={{}}>
+                {/* <div className='d-flex justify-content-center row px-3 py-5' style={{}}>
                     <h3 style={{ color: '#BD10E0' }} className='text-center col-12'>Build and Nurture Your Community Grow</h3>
                     <button type="button" style={{ backgroundColor: '#E9E8F9 ', boxShadow: '2px 2px lightgrey', width: '200px' }} className="btn my-3 text-center mx-auto rounded-pill">Request Early Access<BsArrowRightShort /></button>
+                </div> */}
+
+                <div className='d-flex justify-content-center row mx-lg-5 mx-1 pt-5' style={{ paddingBottom: '100px' }}>
+                    <div className="gradient-box-2 mt-1 my-3">
+                        <div className='my-5 '>
+                            <h3 style={{ color: '#BD10E0' }} className='text-center col-12'>Build and Nurture Your Community Grow</h3>
+                            <button type="button" style={{ backgroundColor: '#E9E8F9', boxShadow: '2px 2px lightgrey', width: '200px' }} className="btn d-flex align-items-center justify-content-between my-3 text-center mx-auto rounded-pill">
+                Request Early Access <BsArrowRightShort />
+            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+
+
     )
 }
 
